d28c283933d79399df6dcee9cf49ae60eca3cf0b
hhstore <hhstore@qq.com>
2016-11-01 16:21:09 +0800

    update - 框架入口.

--- macaron-1.1.8/macaron.go
+++ macaron-1.1.8/macaron.go
@@ -38,11 +38,15 @@ func Version() string {
 	return _VERSION
 }
 
+//-----------------------------------------------------
+// Handler 类型: 空接口
+// 	- 支持传入任意类型, 但要求传入类型为: 可调用函数
 // Handler can be any callable function.
 // Macaron attempts to inject services into the handler's argument list,
 // and panics if an argument could not be fullfilled via dependency injection.
 type Handler interface{}
 
+// 校验 Handler 类型.
 // validateHandler makes sure a handler is a callable function,
 // and panics if it is not.
 func validateHandler(h Handler) {
@@ -51,6 +55,7 @@ func validateHandler(h Handler) {
 	}
 }
 
+// 批量校验
 // validateHandlers makes sure handlers are callable functions,
 // and panics if any of them is not.
 func validateHandlers(handlers []Handler) {
@@ -59,19 +64,23 @@ func validateHandlers(handlers []Handler) {
 	}
 }
 
+//-----------------------------------------------------
+
+
 // Macaron represents the top level web application.
 // inject.Injector methods can be invoked to map services on a global level.
 type Macaron struct {
-	inject.Injector
-	befores  []BeforeHandler
-	handlers []Handler
-	action   Handler
+	inject.Injector              // 反射
+	befores      []BeforeHandler // Handler 钩子
+	handlers     []Handler       // Handler 集合
+	action       Handler
 
-	hasURLPrefix bool
-	urlPrefix    string // For suburl support.
-	*Router
+	hasURLPrefix bool            // 是否有 URL 前缀
+	urlPrefix    string          // For suburl support.
+	*Router                      // 路由模块, 注意Router的定义, (类似链表结构)
+				     // 这里需要理解一下: 是 嵌入, 不是 循环引用, 跟Macaron定义不冲突
 
-	logger *log.Logger
+	logger       *log.Logger     // 日志记录器
 }
 
 // NewWithLogger creates a bare bones Macaron instance.
@@ -104,9 +113,9 @@ func New() *Macaron {
 // mocaron.Logger, mocaron.Recovery and mocaron.Static.
 func Classic() *Macaron {
 	m := New()
-	m.Use(Logger())
-	m.Use(Recovery())
-	m.Use(Static("public"))
+	m.Use(Logger())         // 添加到 handler 列表
+	m.Use(Recovery())       // 添加到 handler 列表
+	m.Use(Static("public")) // 添加到 handler 列表
 	return m
 }
 
@@ -116,33 +125,44 @@ func Classic() *Macaron {
 func (m *Macaron) Handlers(handlers ...Handler) {
 	m.handlers = make([]Handler, 0)
 	for _, handler := range handlers {
-		m.Use(handler)
+		m.Use(handler) // 添加到 handler 列表
 	}
 }
 
 // Action sets the handler that will be called after all the middleware has been invoked.
 // This is set to macaron.Router in a macaron.Classic().
 func (m *Macaron) Action(handler Handler) {
-	validateHandler(handler)
-	m.action = handler
+	validateHandler(handler)  // 校验
+	m.action = handler  // 激活
 }
 
+//*************************************
+// 钩子函数类型定义
+// 在 handler 调用之前执行
 // BeforeHandler represents a handler executes at beginning of every request.
 // Macaron stops future process when it returns true.
 type BeforeHandler func(rw http.ResponseWriter, req *http.Request) bool
 
+// 添加钩子集合:
+// 将所有 `BeforeHandler` 类型的函数钩子, 添加到 befores 列表.
 func (m *Macaron) Before(handler BeforeHandler) {
 	m.befores = append(m.befores, handler)
 }
 
+
+// 关键方法:
 // Use adds a middleware Handler to the stack,
 // and panics if the handler is not a callable func.
 // Middleware Handlers are invoked in the order that they are added.
 func (m *Macaron) Use(handler Handler) {
-	validateHandler(handler)
-	m.handlers = append(m.handlers, handler)
+	validateHandler(handler) // 校验
+	m.handlers = append(m.handlers, handler)  // 添加到方法列表
 }
 
+
+// 请求上下文创建:
+//	- 类似 flask 的上下文概念
+// 	- 这部分代码的实现, 注意阅读, 不好理解
 func (m *Macaron) createContext(rw http.ResponseWriter, req *http.Request) *Context {
 	c := &Context{
 		Injector: inject.New(),
@@ -155,13 +175,15 @@ func (m *Macaron) createContext(rw http.ResponseWriter, req *http.Request) *Cont
 		Render:   &DummyRender{rw},
 		Data:     make(map[string]interface{}),
 	}
-	c.SetParent(m)
+	c.SetParent(m)		// 关键方法
 	c.Map(c)
 	c.MapTo(c.Resp, (*http.ResponseWriter)(nil))
 	c.Map(req)
 	return c
 }
 
+//*************************************
+// 关键方法:
 // ServeHTTP is the HTTP Entry point for a Macaron instance.
 // Useful if you want to control your own HTTP server.
 // Be aware that none of middleware will run without registering any router.
@@ -169,29 +191,36 @@ func (m *Macaron) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	if m.hasURLPrefix {
 		req.URL.Path = strings.TrimPrefix(req.URL.Path, m.urlPrefix)
 	}
+
+	// handler 钩子方法列表不空, 先行执行钩子方法
 	for _, h := range m.befores {
 		if h(rw, req) {
 			return
 		}
 	}
+
+	// 启动服务
 	m.Router.ServeHTTP(rw, req)
 }
 
 func GetDefaultListenInfo() (string, int) {
 	host := os.Getenv("HOST")
 	if len(host) == 0 {
-		host = "0.0.0.0"
+		host = "0.0.0.0"	// 默认 IP
 	}
 	port := com.StrTo(os.Getenv("PORT")).MustInt()
 	if port == 0 {
-		port = 4000
+		port = 4000	// 默认端口
 	}
 	return host, port
 }
 
+//*************************************
+// 模块入口:
+//
 // Run the http server. Listening on os.GetEnv("PORT") or 4000 by default.
 func (m *Macaron) Run(args ...interface{}) {
-	host, port := GetDefaultListenInfo()
+	host, port := GetDefaultListenInfo()	// 获取默认IP+端口
 	if len(args) == 1 {
 		switch arg := args[0].(type) {
 		case string:
@@ -208,10 +237,10 @@ func (m *Macaron) Run(args ...interface{}) {
 		}
 	}
 
-	addr := host + ":" + com.ToStr(port)
+	addr := host + ":" + com.ToStr(port)	// IP + 端口
 	logger := m.GetVal(reflect.TypeOf(m.logger)).Interface().(*log.Logger)
 	logger.Printf("listening on %s (%s)\n", addr, safeEnv())
-	logger.Fatalln(http.ListenAndServe(addr, m))
+	logger.Fatalln(http.ListenAndServe(addr, m))	// 启动监听服务
 }
 
 // SetURLPrefix sets URL prefix of router layer, so that it support suburl.
@@ -228,7 +257,7 @@ func (m *Macaron) SetURLPrefix(prefix string) {
 //                \/              \/    \/          \/     \/
 
 const (
-	DEV  = "development"
+	DEV = "development"
 	PROD = "production"
 	TEST = "test"
 )
@@ -236,7 +265,7 @@ const (
 var (
 	// Env is the environment that Macaron is executing in.
 	// The MACARON_ENV is read on initialization to set this variable.
-	Env     = DEV
+	Env = DEV
 	envLock sync.Mutex
 
 	// Path of work directory.
